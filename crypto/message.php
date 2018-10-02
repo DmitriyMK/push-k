9d1eaac9342e16771115876cb265d5edf962f1b3
DmitriyMK <alchimic@ua.fm>
2018-10-02 13:32:44 +0300

    fx

---
+++ crypto/message.php
@@ -0,0 +1,77 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Crypto Hamster - криптовалютные торговые сигналы</title>
+
+	<meta name="description" content="Crypto Hamster - криптовалютные торговые сигналы">
+	<meta name="keywords" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+	<link rel="mask-icon" href="./images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#121221">
+	<meta name="theme-color" content="#121221">
+
+	<meta property="og:type" content="website"/> 
+	<meta property="og:image" content="photo-og.png">
+	<meta property="og:image:width" content="200">
+	<meta property="og:image:height" content="200">
+	<meta property="og:title" content="Crypto Hamster">
+	<meta property="og:description" content="Криптовалютные торговые сигналы">
+	
+	
+	<link rel="preload" href="./css/slick-theme.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="./css/slick.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="./css/remodal-default-theme.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="./css/remodal.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="./css/animate.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+
+	<noscript>
+		<link rel="stylesheet" href="./css/slick-theme.css">
+		<link rel="stylesheet" href="./css/slick.css">
+		<link rel="stylesheet" href="./css/remodal-default-theme.css">
+		<link rel="stylesheet" href="./css/remodal.css">
+		<link rel="stylesheet" href="./css/animate.min.css">
+	</noscript>
+
+	<link rel="stylesheet" href="./css/style.css">
+
+
+	<!-- DEFER SCRIPT -->
+	<script defer src="./libs/slick.js"></script>
+	<script defer src="./libs/cssrelpreload.js"></script>
+	<!-- END DEFER SCRIPT -->
+
+</head>
+<body>
+	
+	<div class="message">
+		<p>РКН блокирует Телеграм, потому ссылки на него не открываются.</p>
+		<p>Если через пару секунд у этой страницы не получится открыть приложение Телеграма на вашем устройстве, попробуйте нажать на ссылку: <a class="link__accent" href="https://t.me/CryptoHamsterBot">@CryptoHamsterBot</a></p>
+		<p>Возможно у вас Телеграм не заблокирован до конца, и всё заработает.</p>
+		<p>Если нет, то вам остаётся только искать мой канал в приложении самостоятельно.</p>
+	</div>	
+
+	<script>
+		window.location.href = '<?= $_GET['url']; ?>';//'tg://resolve?domain=CryptoHamsterBot'
+	</script>
+
+
+	<!-- Load Scripts -->
+	<script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script src="./libs/jquery.nicescroll.min.js"></script>
+	<script src="./libs/blazy.js"></script>  
+	<script src="./libs/wow.min.js"></script>  
+	<script src="./libs/remodal.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+	<script src="//cdn.rawgit.com/martinlaxenaire/curtainsjs/master/build/curtains.min.js"></script>
+
+	<script src="./libs/all.js"></script>
+
+</body>
+</html>
