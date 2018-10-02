e10c8d191317729762f8a404f96246eeba6202f9
DmitriyMK <alchimic@ua.fm>
2018-10-02 18:38:18 +0300

    fx

--- antistress3/app/js/script.js
+++ antistress3/app/js/script.js
@@ -1,157 +1,30 @@
 
-/*PRELOADER*/
-$(window).load(function() {
-    $('#preloader').delay(100).fadeOut('slow');
-});
+var options = {
+    paths: '#round',     // Shape we want to draw
+    pointsNumber: 10,            // Number of points
+    maxDistance: 100,             // Max distance among points
+    color: 'red',
+    // debug: true               // Uncomment this to see the points
+};
 
-$(document).ready(function() {
+/* Initializing jelly */
 
-
-/*  if (document.documentElement.clientWidth > 1200) {
-
-    $(window).enllax();
-
-    new WOW().init();
-
-    $("body").niceScroll({
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      smoothscroll: true,
-      cursorwidth: 8,
-      cursorborder: 0,
-      cursorcolor: '#ff4200',
-      cursorborderradius: 2,
-      autohidemode: true,
-      horizrailenabled: false,
-      cursoropacitymin: 1,
-      background: false,
-      zindex: "9999"
-    });
-
-    $('html').addClass('no-overflow-y');
-
-  };*/
-
-
-/*  $('.scrolling__link').on('click', function(e) {
-    e.preventDefault();
-    var t = 1500;
-    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-    $('html,body').stop().animate({
-      scrollTop: $(d).offset().top
-    }, t);
-  });*/
-
-  // var bLazy = new Blazy();
-
-  // $('.phone-mask').mask('+38(999)999-99-99');
-
-
-/*  $(".js-video-button").modalVideo({
-    youtube: {
-      autoplay: 1,
-      nocookie: false,
-      cc_load_policy: 1,
-      color: null,
-      controls: 1,
-      disablekb: 0,
-      enablejsapi: 0,
-      end: null,
-      fs: 1,
-      h1: null,
-      iv_load_policy: 1,
-      list: null,
-      listType: null,
-      loop: 0,
-      modestbranding: null,
-      origin: null,
-      playlist: null,
-      playsinline: null,
-      rel: 0,
-      showinfo: 1,
-      start: 0,
-      wmode: 'transparent',
-      theme: 'dark'
-
-    }
-  });*/
+var jelly = new Jelly('.jelly', options);
 
 
 
-/*  $('.slider-for-recipe').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    asNavFor: '.slider-nav-recipe',
-    fade: true,
-    draggable: false,
-    infinite: true,
+// function animLogo() {
+//   anime({
+//     targets: '#round',
+//     d: "M 259.04104,0.01442282 C 164.4263,0.58553355 59.295072,15.616505 24.262606,100.36045 c -30.1160556,69.92572 -35.784833,154.81232 3.645885,222.38352 44.41051,79.4652 128.449739,153.49995 217.248239,153.5676 92.15978,0.9989 176.55777,-0.12245 232.97921,-76.86598 C 513.677,352.3599 520.18777,288.24523 500.85725,233.26065 464.63177,122.84026 413.15536,25.533345 298.49961,3.4277534 285.48899,1.0783349 272.26006,-0.14877975 259.04104,0.01442282 Z",
+//     duration: 3000,
+//     loop: true,
+//     direction: "alternate",
+//     easing: [.52, .04, .55, 1],
+//     elasticity: function(el, i, l) {
+//       return (200 + i * 200);
+//     }
+//   });
+// }
 
-  });*/
-
-
-/*  $('.slider-nav-recipe').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for-recipe',
-    arrows: false,
-    dots: false,
-    focusOnSelect: true,
-    infinite: true,
-    centerMode: false,
-
-    responsive: [
-
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: true,
-
-      }
-    },
-
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-
-        arrows: true,
-      }
-    },
-
-    ]
-
-  });
-*/
-
-});
-
-
-
-// $(window).on('resize orientationchange', function() {
-//   $('.slider').slick('resize');
-// });
-
-
-
-$(".form").submit(function() {
-  var th = $(this);
-  $.ajax({
-    type: "POST",
-    url: "mail.php",
-    data: th.serialize()
-  }).done(function() {
-
-    var inst = $('[data-remodal-id=modal-thanks]').remodal();
-    inst.open();
-
-    setTimeout(function() {
-      th.trigger("reset");
-    }, 1000);
-  });
-  return false;
-});
+// animLogo();
