59698b8bf84b4aa6d3bd0dae859483cd51d5e172
DmitriyMK <alchimic@ua.fm>
2018-10-10 19:11:34 +0300

    add new proj

--- 4paws_gallery/client_build/libs/all.js
+++ 4paws_gallery/client_build/libs/all.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
 
   if (document.documentElement.clientWidth > 1200) {
-
     $(window).enllax();
-
-    $("body").niceScroll({
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      smoothscroll: true,
-      cursorwidth: 8,
-      cursorborder: 0,
-      cursorcolor: '#fe673a',
-      cursorborderradius: 2,
-      autohidemode: true,
-      horizrailenabled: false,
-      cursoropacitymin: 1,
-      background: false,
-      zindex: "9999"
-    });
-
-    $('html').addClass('no-overflow-y');
-
   };
 
 
