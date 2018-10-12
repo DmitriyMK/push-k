487f88c96101b3994b0bee0273ff73ca86586736
DmitriyMK <alchimic@ua.fm>
2018-10-12 18:13:57 +0300

    fx

--- stimlex/client_build/libs/all.js
+++ stimlex/client_build/libs/all.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     event.preventDefault();
     var id = $(this).attr('href'),
 
-      top = $(id).offset().top;
+    top = $(id).offset().top;
 
     $('body,html').animate({
       scrollTop: top
@@ -41,107 +41,51 @@ $(document).ready(function() {
 
   $('.pots__materials button').click(function() {
 
-      if (!$(this).parents('.pots__btn').hasClass('pots__btn-active')) {
-        $(this).parents('.pots__materials').find('.pots__btn').removeClass('pots__btn-active');
-        $(this).addClass('pots__btn-active');
+    if (!$(this).parents('.pots__btn').hasClass('pots__btn-active')) {
+      $(this).parents('.pots__materials').find('.pots__btn').removeClass('pots__btn-active');
+      $(this).addClass('pots__btn-active');
 
-        $(this).parents('.pots__item').find(".pots__link.pots__link-active").removeClass("pots__link-active");
-        $('#' + $(this).data('model')).addClass('pots__link-active');
+      $(this).parents('.pots__item').find(".pots__link.pots__link-active").removeClass("pots__link-active");
+      $('#' + $(this).data('model')).addClass('pots__link-active');
 
-        $(this).parents('.pots__item').find('.pricePots').html($(this).data('price'));
+      $(this).parents('.pots__item').find('.pricePots').html($(this).data('price'));
 
-        $('.form input[name="whatform2"]').val($(this).data('material'));
-      }
+      $('.form input[name="whatform2"]').val($(this).data('material'));
+    }
 
-      return false;
+    return false;
   });
-
 
 
 
   $('.phone-mask').mask('+39(999)999-99-99');
 
 
-  /*  $(".js-video-button").modalVideo({
-      youtube: {
-        autoplay: 1,
-        nocookie: false,
-        cc_load_policy: 1,
-        color: null,
-        controls: 1,
-        disablekb: 0,
-        enablejsapi: 0,
-        end: null,
-        fs: 1,
-        h1: null,
-        iv_load_policy: 1,
-        list: null,
-        listType: null,
-        loop: 0,
-        modestbranding: null,
-        origin: null,
-        playlist: null,
-        playsinline: null,
-        rel: 0,
-        showinfo: 1,
-        start: 0,
-        wmode: 'transparent',
-        theme: 'dark'
 
+  $('.review__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    lazyLoad: 'ondemand',
+    arrows: true,
+    dots: true,
+    infinite: true,
+
+    responsive: [
+
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        arrows: true,
       }
-    });*/
+    },
 
+    ]
 
+  });
 
-  /*  $('.slider-for-recipe').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: false,
-      asNavFor: '.slider-nav-recipe',
-      fade: true,
-      draggable: false,
-      infinite: true,
-
-    });*/
-
-
-  /*  $('.slider-nav-recipe').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for-recipe',
-      arrows: false,
-      dots: false,
-      focusOnSelect: true,
-      infinite: true,
-      centerMode: false,
-
-      responsive: [
-
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: true,
-
-        }
-      },
-
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-
-          arrows: true,
-        }
-      },
-
-      ]
-
-    });
-    */
 
   $('[data-fancybox]').fancybox({
     loop: true,
@@ -162,9 +106,9 @@ $(document).ready(function() {
 
 
 
-// $(window).on('resize orientationchange', function() {
-//   $('.slider').slick('resize');
-// });
+$(window).on('resize orientationchange', function() {
+  $('.slider').slick('resize');
+});
 
 
 
