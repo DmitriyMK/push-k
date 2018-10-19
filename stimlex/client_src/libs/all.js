2fc3836cdd16d4a6d866d467e20e56d57fa40f99
DmitriyMK <alchimic@ua.fm>
2018-10-19 11:22:29 +0300

    fx

--- stimlex/client_src/libs/all.js
+++ stimlex/client_src/libs/all.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 
-  // if (document.documentElement.clientWidth > 1200) {
+  if (document.documentElement.clientWidth > 1200) {
 
-  //   $("body").niceScroll({
-  //     scrollspeed: 60,
-  //     mousescrollstep: 60,
-  //     smoothscroll: true,
-  //     cursorwidth: 8,
-  //     cursorborder: 0,
-  //     cursorcolor: '#6b6c70',
-  //     cursorborderradius: 2,
-  //     autohidemode: true,
-  //     horizrailenabled: false,
-  //     cursoropacitymin: 1,
-  //     background: false,
-  //     zindex: "9999"
-  //   });
+    $("body").niceScroll({
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      smoothscroll: true,
+      cursorwidth: 8,
+      cursorborder: 0,
+      cursorcolor: '#6b6c70',
+      cursorborderradius: 2,
+      autohidemode: true,
+      horizrailenabled: false,
+      cursoropacitymin: 1,
+      background: false,
+      zindex: "9999"
+    });
 
-  //   $('html').addClass('no-overflow-y');
+    $('html').addClass('no-overflow-y');
 
-  // };
+  };
 
 
   $(".scrolling__link").on("click", function(event) {
