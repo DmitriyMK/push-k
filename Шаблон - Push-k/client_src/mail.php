f1d378c6ab3581bfd2ade44ffa3def3731a8250d
DmitriyMK <alchimic@ua.fm>
2018-11-01 10:23:21 +0200

    delete old

--- Шаблон - Push-k/client_src/mail.php
+++ Шаблон - Push-k/client_src/mail.php
@@ -32,9 +32,6 @@ if ( $method === 'POST' ) {
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
 		</tr>
 		";
 	}
