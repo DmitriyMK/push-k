09d6bb86a83758e218962b7e74319064e5bbd34b
DmitriyMK <alchimic@ua.fm>
2018-11-13 18:07:04 +0200

    add Glitch

--- pushK/client_build/libs/all.js
+++ pushK/client_build/libs/all.js
@@ -20,6 +20,84 @@ $(document).ready(function() {
   var bLazy = new Blazy();
 
 
+
+  //GLITCH
+  
+  var tl = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 2
+  });
+
+  tl.to('.glitch', 0.1, {
+      skewX: 50,
+      ease: Power4.easeInOut
+    })
+    .to('.glitch', 0.04, {
+      skewX: 0,
+      ease: Power4.easeInOut
+    })
+    .to('.glitch', 0.04, {
+      opacity: 0
+    })
+    .to('.glitch', 0.04, {
+      opacity: 1
+    })
+    .to('.glitch', 0.04, {
+      x: -20
+    })
+    .to('.glitch', 0.04, {
+      x: 0
+    })
+    .add("split", 0)
+    .to('.top', 0.5, {
+      x: -20,
+      ease: Power4.easeInOut
+    }, 'split')
+    .to('.bottom', 0.5, {
+      x: 60,
+      ease: Power4.easeInOut
+    }, 'split')
+    .to('.glitch', 0.08, {
+      className: '+=redShadow'
+    }, 'split')
+
+  .to('#txt', 0, {
+      scale: 1.1
+    }, 'split')
+    .to('#txt', 0, {
+      scale: 1
+    }, "+=0.02")
+
+  .to('.glitch', 0.08, {
+      className: '-=redShadow'
+    }, "+=0.09")
+    .to('.glitch', 0.03, {
+      className: '+=greenShadow'
+    }, 'split')
+    .to('.glitch', 0.03, {
+      className: '-=greenShadow'
+    }, "+=0.01")
+
+  .to('.top', 0.2, {
+      x: 0,
+      ease: Power4.easeInOut
+    })
+    .to('.bottom', 0.2, {
+      x: 0,
+      ease: Power4.easeInOut
+    })
+
+  .to('.glitch', 0.02, {
+      scaleY: 1.1,
+      ease: Power4.easeInOut
+    })
+    .to('.glitch', 0.04, {
+      scaleY: 1,
+      ease: Power4.easeInOut
+    })
+
+
+
   //   var options = {
   //     paths: '#round',     // Shape we want to draw
   //     pointsNumber: 10,            // Number of points
@@ -35,10 +113,8 @@ $(document).ready(function() {
 
   $('.phone-mask').mask('+380(99)999-99-99');
 
-  //GLITCH
-  //
-  //
   
+
   // BUBBLE ANIMATION for BUTTON
   var animateButton = function(e) {
     e.preventDefault;
