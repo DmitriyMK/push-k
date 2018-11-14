296f850aaaa8533e28c01ecd5492b8cc3f506c91
Dmitriy (alchimic) <alchimic@ua.fm>
2018-11-14 20:45:55 +0200

    add glitch

---
+++ pushKGlitchSuper/client_build/libs/map.js
@@ -0,0 +1,181 @@
+var markersData = [{
+    lat: 50.4301475,
+    lng: 30.4737761,
+    name: "PUSH-K Solutions",
+    address: "г. Киев, ул. Соломенская, 3 офис 205"
+}, ];
+
+var map, infoWindow;
+
+function initMap() {
+    var centerLatLng = new google.maps.LatLng(50.4301475, 30.4737761);
+
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 15.5,
+
+        mapTypeControl: false,
+
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles:
+
+            [{
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e9e9e9"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f5f5f5"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 29
+            }, {
+                "weight": 0.2
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 18
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f5f5f5"
+            }, {
+                "lightness": 21
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dedede"
+            }, {
+                "lightness": 21
+            }]
+        }, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#ffffff"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "saturation": 36
+            }, {
+                "color": "#333333"
+            }, {
+                "lightness": 40
+            }]
+        }, {
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f2f2f2"
+            }, {
+                "lightness": 19
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#fefefe"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#fefefe"
+            }, {
+                "lightness": 17
+            }, {
+                "weight": 1.2
+            }]
+        }]
+
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < markersData.length; i++) {
+
+        var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+        var name = markersData[i].name;
+        var address = markersData[i].address;
+
+        addMarker(latLng, name, address);
+
+        bounds.extend(latLng);
+    }
+
+    // map.fitBounds(bounds);
+
+}
+
+function addMarker(latLng, name, address) {
+
+    var image = 'images/marker.png';
+
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        icon: image,
+        title: name
+    });
+
+
+}
