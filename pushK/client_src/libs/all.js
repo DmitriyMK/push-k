1b2c30f88e2d4684f50ff3eabb2a41d32cfde6df
DmitriyMK <alchimic@ua.fm>
2018-11-20 11:07:49 +0200

    add cursor - Push-K

--- pushK/client_src/libs/all.js
+++ pushK/client_src/libs/all.js
@@ -340,37 +340,37 @@ $(function() {
 
 
 		$('[data-scrollmagic1]').each(function(index, elem) {
-		var scrollMagicController = new ScrollMagic();
+			var scrollMagicController = new ScrollMagic();
 
-		var scene = new ScrollScene({
-			triggerElement: '#portfolio',
-			reverse: true,
-			duration: 400,
-			pushFollowers: false,
-			offset: 360
-		})
+			var scene = new ScrollScene({
+				triggerElement: '#portfolio',
+				reverse: true,
+				duration: 400,
+				pushFollowers: false,
+				offset: 360
+			})
 
-		.setPin("#portfolioContent")
-		.addTo(scrollMagicController);
-		scene.addIndicators();
+			.setPin("#portfolioContent")
+			.addTo(scrollMagicController);
+			scene.addIndicators();
 		});
 
 
 		$('[data-scrollmagic31]').each(function(index, elem) {
 
-		var scrollMagicController = new ScrollMagic();
+			var scrollMagicController = new ScrollMagic();
 
-		var scene = new ScrollScene({
-			triggerElement: '#team',
-			reverse: true,
-			duration: 700,
-			pushFollowers: false,
-			offset: 460
-		})
+			var scene = new ScrollScene({
+				triggerElement: '#team',
+				reverse: true,
+				duration: 700,
+				pushFollowers: false,
+				offset: 460
+			})
 
-		.setPin("#teamContent")
-		.addTo(scrollMagicController);
-		scene.addIndicators();
+			.setPin("#teamContent")
+			.addTo(scrollMagicController);
+			scene.addIndicators();
 		});
 		
 
@@ -567,6 +567,77 @@ $(window).scroll(function() {
 		$('.totop a').fadeOut();
 	}
 });
+
+
+// dots is an array of Dot objects,
+// mouse is an object used to track the X and Y position
+   // of the mouse, set with a mousemove event listener below
+   var dots = [],
+   mouse = {
+   	x: 0,
+   	y: 0
+   };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+	this.x = 0;
+	this.y = 0;
+	this.node = (function(){
+		var n = document.createElement("div");
+		n.className = "cursor";
+		document.body.appendChild(n);
+		return n;
+	}());
+};
+// The Dot.prototype.draw() method sets the position of 
+  // the object's <div> node
+  Dot.prototype.draw = function() {
+  	this.node.style.left = this.x + -16 + "px";
+  	this.node.style.top = this.y + -16 + "px";
+  };
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 1; i++) {
+	var d = new Dot();
+	dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+  // Make sure the mouse position is set everytime
+    // draw() is called.
+    var x = mouse.x,
+    y = mouse.y;
+
+  // This loop is where all the 90s magic happens
+  dots.forEach(function(dot, index, dots) {
+  	var nextDot = dots[index + 1] || dots[0];
+
+  	dot.x = x;
+  	dot.y = y;
+  	dot.draw();
+  	x += (nextDot.x - dot.x) * .6;
+  	y += (nextDot.y - dot.y) * .6;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  //event.preventDefault();
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+// animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+  function animate() {
+  	draw();
+  	requestAnimationFrame(animate);
+  }
+
+// And get it started by calling animate().
+animate();
+
 
 
 $(".form").submit(function() {
