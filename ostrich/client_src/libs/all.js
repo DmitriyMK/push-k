dcc4f176d0ef707c8af50c40503b81575a302ad0
DmitriyMK <alchimic@ua.fm>
2018-11-23 14:02:19 +0200

    fix calc - PushK

--- ostrich/client_src/libs/all.js
+++ ostrich/client_src/libs/all.js
@@ -15,4 +15,35 @@ $(document).ready(function() {
 
   $('.phone-mask').mask('+38(999)999-99-99');
 
+
+  $(".btn__minus").click(function() {
+    var $this = $(this);
+    var quantity = parseInt($this.siblings(".accordion__num").val());
+    if (quantity > 0) 
+      quantity--; 
+    $(this).siblings(".accordion__num").val(quantity);
+    calcPrice();
+  });
+
+
+  $(".btn__plus").click(function() {
+    var $this = $(this);
+    var quantity = parseInt($this.siblings(".accordion__num").val()) + 1;
+    $(this).siblings(".accordion__num").val(quantity);
+    calcPrice();
+  });
+
+
+
+  var calcPrice = function() {
+    var sumPrice = $(".sumPrice");
+
+    var priceAdult = parseInt($('.priceAdult').html());
+    var quantityAdult = parseInt($(".amountTicketsAdults").val());
+    var priceChildren = parseInt($(".priceChildren").html());
+    var quantityChildren = parseInt($(".amountTicketsChildren").val());
+
+    var totalPrice = priceAdult * quantityAdult + priceChildren * quantityChildren;
+    sumPrice.html(totalPrice);
+  };
 });
