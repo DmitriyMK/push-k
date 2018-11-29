5921561710080f36e2a63c337166e6fd84b22f49
DmitriyMK <alchimic@ua.fm>
2018-11-29 17:12:39 +0200

    add shaders for background - Luxury

--- luxury/client_build/libs/all.js
+++ luxury/client_build/libs/all.js
@@ -1,6 +1,34 @@
+window.onload = function() {
+  var canvasContainer = document.getElementById("canvas");
+  var webGLCurtain = new Curtains("canvas");
+
+  var planeElement = document.getElementsByClassName("hero__animation")[0];
+
+  var params = {
+    vertexShaderID: "plane-vs",
+    fragmentShaderID: "plane-fs", 
+
+    uniforms: {
+      time: {
+        name: "uTime",
+        type: "1f", 
+        value: 0,
+      },
+    }
+  }
+
+  var plane = webGLCurtain.addPlane(planeElement, params);
+
+  plane.onRender(function() {
+    plane.uniforms.time.value++;
+  });
+};
+
+
 $(document).ready(function() {
 
-  var cardWrap = document.getElementsByClassName('hero__img');
+
+  var cardWrap = document.getElementsByClassName('hero__content');
   document.body.addEventListener('mousemove', cursorPositionHandler);
 
 
@@ -18,12 +46,83 @@ $(document).ready(function() {
   };
 
 
+
+  let els = document.querySelectorAll(".js-splitme");
+  [].forEach.call(els, function(el) {
+    el.outerHTML = Splitter(el.outerHTML, '<span class="letter">$</span>');
+  });
+
+
+
   var tl = new TimelineMax();
   tl
-  .fromTo(".header__box", 1, {y: -100, opacity: 0}, {y: 0,opacity: 1})
-  .fromTo(".nav", 1, {y: -50}, {y: 0}, 0.03)
 
+  .fromTo(".header__logo", 1.3, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Back.easeOut.config(3)}, 1.2)
+  .fromTo(".header__btn", 1.2, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Back.easeOut.config(3)}, 1.5)
+  // .staggerFromTo(".hero__title .letter", 0.3, {y: 30, opacity: 0}, {y: 0, opacity: 1, ease: Back.easeOut.config(2)}, 0.1);
+  .fromTo(".hero__title", .8, {x: -50, opacity: 0}, {x: 0, opacity: 1, ease: Back.easeOut.config(3)}, 0.9)
+
+
+
+//CURSOR CIRCLE
+  $(function() {
+    var dots = [],
+      mouse = {
+        x: 0,
+        y: 0
+      };
+
+    var Dot = function() {
+      this.x = 0;
+      this.y = 0;
+      this.node = (function() {
+        var n = document.createElement("div");
+        n.className = "cursor";
+        document.body.appendChild(n);
+        return n;
+      }());
+    };
+
+    Dot.prototype.draw = function() {
+      this.node.style.left = this.x + -16 + "px";
+      this.node.style.top = this.y + -16 + "px";
+    };
+
+    for (var i = 0; i < 1; i++) {
+      var d = new Dot();
+      dots.push(d);
+    }
+
+    function draw() {
+      var x = mouse.x,
+          y = mouse.y;
+
+      dots.forEach(function(dot, index, dots) {
+        var nextDot = dots[index + 1] || dots[0];
+
+        dot.x = x;
+        dot.y = y;
+        dot.draw();
+        x += (nextDot.x - dot.x) * .16;
+        y += (nextDot.y - dot.y) * .16;
+
+      });
+    }
+
+    addEventListener("mousemove", function(event) {
+      mouse.x = event.pageX;
+      mouse.y = event.pageY;
+    });
+
+    function animate() {
+      draw();
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+  });
 });
+
 
 
 
@@ -45,6 +144,7 @@ $(".form").submit(function() {
   });
   return false;
 });
+
 
 
 window.console.log('Made with fun and love ❤️ Push-K solutions ❤️');
