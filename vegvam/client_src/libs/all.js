dc41a87aa1dd03b789984e26c286978bc2f6ae57
Dmitriy (alchimic) <alchimic@ua.fm>
2018-12-15 13:20:36 +0200

    fx

--- vegvam/client_src/libs/all.js
+++ vegvam/client_src/libs/all.js
@@ -321,6 +321,18 @@ $(window).scroll(function() {
 });
 
 
+$(".scrollingLink").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+
+    top = $(id).offset().top;
+
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+
 
 $("#feedbackForm").validate({
 
